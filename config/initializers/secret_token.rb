2eb216b8d48b5587eb1b200f45b23a2920bd2dc8
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-31 15:18:42 -0400

    Omnium rerum principia parva sunt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinAnalytics::Application.config.secret_token = '2893d3527d57e6faf8f345c44684a9f6fa37ac872f9cc267a6d17bfcec63fd387eee3aaca886bf7c7086fe099c6038fbe29f3cda7eb13b4c0f3741c7f6334bc5'
